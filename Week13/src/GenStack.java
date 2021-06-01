641c28e4c2f99f70111fce2ab5b1215f72fcf088
Slevious <83533209+Slevious@users.noreply.github.com>
2021-06-02 06:23:22 +0900

    Add files via upload

---
+++ Week13/src/GenStack.java
@@ -0,0 +1,22 @@
+//public class GenStack<T> {
+//    //Gen관련 이용
+//    private Object[] stack;
+//    private int top;
+//
+//    public GenStack(int size){
+//        stack = new Object[size];
+//        top = 0;
+//    }
+//
+//    public void push(T item){
+//        stack[top++] = item;
+//    }
+//
+//    public T pop() {
+//        return (T) stack[--top];
+//    }
+//
+//    public boolean isEmpty(){
+//        return (top == 0);
+//    }
+//}
